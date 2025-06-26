12610da6529a45c3be7da86e28b2275227e24789
Sakib <sakibhossainkhankaptai007@gmail.com>
2025-06-26 20:28:31 +0600

    npm pakage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
 import React from "react";
+import "antd/dist/reset.css";
+import { Button } from "antd";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      App
+      <Button type="primary">Click Me</Button>
+    </div>
+  );
 }
